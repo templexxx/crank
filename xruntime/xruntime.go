f4165ae9ce9845c273f0861cc5b4288cdbc5c749
triplex <lucas1x1x@gmail.com>
2023-07-29 20:12:54 +0800

    xruntime: add ProcYield properties testing

--- xruntime/xruntime.go
+++ xruntime/xruntime.go
@@ -1,10 +1,9 @@
 package xruntime
 
 // ProcYield yields logic processor for a while (decided by cycles).
+// Providing stable and low-energy intervals for spin scenarios (e.g. spin lock).
 //
-// e.g. when n = 1, it'll cost 30-40ns.
-// If you want 400ns wait, n could be 10.
-// (CPU-specific dependencies)
+// The cost is CPU-specific dependencies.
 //
 // It's not a good idea to use time.Sleep as ProcYield, because it may cause
 // goroutine being scheduled and the cost will be unpredictable.
@@ -12,5 +11,7 @@ func ProcYield(cycles uint32) {
 	pyield(cycles)
 }
 
-// TODO func ProcYieldNS(nanoseconds int64) {}
+// TODO func ProcYieldNS(nanoseconds int64) {} will it work?
 // nanoseconds -> cycles (depends on CPU family which detected in init)
+
+// TODO DoSpins (with default cycles for ProcYield)
--- xruntime/xruntime_test.go
+++ xruntime/xruntime_test.go
@@ -1,12 +1,71 @@
 package xruntime
 
-import "testing"
+import (
+	"os/exec"
+	"runtime"
+	"strings"
+	"testing"
 
-func BenchmarkDoNothing(b *testing.B) {
+	"github.com/klauspost/cpuid/v2"
+	"github.com/templexxx/crank/xtest"
+	"github.com/templexxx/tsc"
+)
 
-	for i := 0; i < b.N; i++ {
-		ProcYield(1000)
+func TestProcYield(t *testing.T) {
+
+	if !xtest.IsPropEnabled() {
+		t.Skip("properties testing isn't enabled")
+	}
+
+	cs := []int{10, 20, 30, 60, 120, 240}
+
+	for _, c := range cs {
+		// TODO using TSC register to report cost (as property testing)
+		tsc.ForbidOutOfOrder()
+		start := tsc.UnixNano()
+		for i := 0; i < 1000; i++ {
+			ProcYield(uint32(c))
+		}
+		cost := (tsc.UnixNano() - start) / 1000
+		t.Logf("%d cycles cost: %dns on %s", c, cost, getCPUBrand())
 	}
 }
 
-// TODO using TSC register to report cost (as property testing)
+func getCPUBrand() string {
+
+	brand := cpuid.CPU.BrandName
+	if brand != "" {
+		return cpuid.CPU.BrandName
+	}
+
+	if runtime.GOOS == `darwin` {
+		brand = getCPUBrandOnDarwin()
+	}
+	if brand == "" {
+		return "unknown"
+	}
+	return brand
+}
+
+func getCPUBrandOnDarwin() string {
+	grep := exec.Command("grep", "machdep.cpu.brand_string")
+	sysctl := exec.Command("sysctl", "-a")
+
+	pipe, err := sysctl.StdoutPipe()
+	if err != nil {
+		return ""
+	}
+	defer pipe.Close()
+
+	grep.Stdin = pipe
+
+	err = sysctl.Start()
+	if err != nil {
+		return ""
+	}
+	res, err := grep.Output()
+	if err != nil {
+		return ""
+	}
+	return strings.Split(string(res), ": ")[1]
+}
