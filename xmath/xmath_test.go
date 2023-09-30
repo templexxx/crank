9b7421e8cc2365af97c4d5d29e62a197ddda385a
triplex <lucas1x1x@gmail.com>
2023-09-30 23:59:57 +0800

    test(xmath): refine Round test

--- xmath/xmath_test.go
+++ xmath/xmath_test.go
@@ -9,12 +9,18 @@ func TestRound(t *testing.T) {
 	var i float64
 	for i = 0; i < 0.05; i += 0.01 {
 		if Round(f+i, 1) != 1.1 {
-			t.Fatal("mismatch")
+			testRound(t, f+i, 1.1, Round(f+i, 1), 1)
 		}
 	}
 	for i = 0.05; i < 0.1; i += 0.01 {
 		if Round(f+i, 1) != 1.2 {
-			t.Fatal("mismatch")
+			testRound(t, f+i, 1.2, Round(f+i, 1), 1)
 		}
+	}
+}
+
+func testRound(t *testing.T, input, exp, got float64, decimal int) {
+	if exp != got {
+		t.Fatalf("mismatch: input=%f, exp=%f, got=%f, decimal=%d", input, exp, got, decimal)
 	}
 }
