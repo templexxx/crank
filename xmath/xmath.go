61cfaf15d97872bb85bef4c2207c5cf7b3acc544
triplex <lucas1x1x@gmail.com>
2023-10-04 12:14:28 +0800

    feat(xmath): add NextPow2

--- xmath/xmath.go
+++ xmath/xmath.go
@@ -3,6 +3,7 @@ package xmath
 
 import (
 	"math"
+	"math/bits"
 )
 
 // Round rounds a float64 and cuts it by n.
@@ -12,4 +13,14 @@ import (
 func Round(f float64, n int) float64 {
 	pow10n := math.Pow10(n)
 	return math.Trunc(f*pow10n+0.5) / pow10n
+}
+
+// NextPow2 returns the next power of 2 that is greater than or equal to the input number n.
+// For example, if n is 1, it will return 1. If n is 2, it will return 2. If n is 3, it will return 4.
+func NextPow2(n uint64) uint64 {
+	if n <= 1 {
+		return 1
+	}
+
+	return 1 << (64 - bits.LeadingZeros64(n-1)) // TODO may use BSR instruction.
 }
--- xmath/xmath_test.go
+++ xmath/xmath_test.go
@@ -24,3 +24,34 @@ func testRound(t *testing.T, input, exp, got float64, decimal int) {
 		t.Fatalf("mismatch: input=%f, exp=%f, got=%f, decimal=%d", input, exp, got, decimal)
 	}
 }
+
+func TestNextPow2(t *testing.T) {
+
+	testNextPow2(t, 0, 1, NextPow2(0))
+	testNextPow2(t, 1, 1, NextPow2(1))
+	testNextPow2(t, 2, 2, NextPow2(2))
+	testNextPow2(t, 3, 4, NextPow2(3))
+	testNextPow2(t, 4, 4, NextPow2(4))
+
+	for i := 5; i <= 1025; i++ {
+		testNextPow2(t, uint64(i), slowNextPow2(uint64(i)), NextPow2(uint64(i)))
+	}
+}
+
+func slowNextPow2(n uint64) uint64 {
+	var p uint64 = 1
+	for {
+		if p < n {
+			p *= 2
+		} else {
+			break
+		}
+	}
+	return p
+}
+
+func testNextPow2(t *testing.T, n, exp, got uint64) {
+	if exp != got {
+		t.Fatalf("mismatch: n=%d, exp=%d, got=%d", n, exp, got)
+	}
+}
